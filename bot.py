4a9a54939b9080f18980fc3e4ed32bbce77b50c6
vangu <vanguskov@gmail.com>
2022-03-24 23:14:55 +0700

    test commit

--- bot.py
+++ bot.py
@@ -2,6 +2,8 @@
 # pylint: disable=C0116,W0613
 # This program is dedicated to the public domain under the CC0 license.
 
+# wtf
+
 """
 Simple Bot to reply to Telegram messages.
 
@@ -133,7 +135,8 @@ def echo(update: Update, context: CallbackContext) -> None:
 def main() -> None:
     """Start the bot."""
     # Create the Updater and pass it your bot's token.
-    updater = Updater("5040799832:AAF9F8KTQr6CuAKKz8ciWkBuKxQHuOBrTcI")
+    # updater = Updater("5040799832:AAF9F8KTQr6CuAKKz8ciWkBuKxQHuOBrTcI")
+    updater = Updater("5043534943:AAGNjukWrf9a-u1f-33wN6sF09N9WPwzdl0")
 
     # Get the dispatcher to register handlers
     dispatcher = updater.dispatcher
@@ -166,7 +169,6 @@ def main() -> None:
     # SIGTERM or SIGABRT. This should be used most of the time, since
     # start_polling() is non-blocking and will stop the bot gracefully.
     updater.idle()
-
 
 if __name__ == '__main__':
     main()
