ae836c3b78b7659507ade97ba95282858edcd015
yguskov <yguskov@rssystems.ru>
2022-04-07 16:46:48 +0700

    - add example html file - update parser and save grades as subjects dict of the grades dict by dates

--- schoolparse.py
+++ schoolparse.py
@@ -1,13 +1,24 @@
 from bs4 import BeautifulSoup as bs
 import requests
 
-html_nso = requests.get('C:\Users\vangu\schoolbot\School.html')
-soup_nso = bs(html_nso.text, 'html.parser')
+from pathlib import Path
 
-mean_grades = []
+# html_nso = requests.get('https://school.nso.ru/journal-student-grades-action/u.2169')
+html_nso = Path('grades_example.html').read_text()
 
-for i in soup_nso.select('#g0_avg'):
-    mn_gr = soup_nso.select('.cell')
-    mean_grades.append(mn_gr)
+soup_nso = bs(html_nso, 'html.parser')
 
-print(mean_grades)
+grades = dict()
+print(len(soup_nso.select('div[mark_date]')))
+for domElement in soup_nso.select('div[mark_date]'):
+    if domElement.get('mark_date') != "":
+        if domElement.get('name') in grades:
+            grades[domElement.get('name')][domElement.get('mark_date')] = domElement.get_text('', True)
+        else:
+            grades[domElement.get('name')] = dict()
+    # mn_gr = soup_nso.select('.cell')
+    # mean_grades.append(mn_gr)
+
+for subject in grades:
+    print(grades[subject])
+
